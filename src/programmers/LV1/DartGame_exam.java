e0460742c001bb6d24efe22e3e3f486671487c83
Ju Minji <alswl0w0@gmail.com>
2022-11-19 19:03:49 +0900

    Add DartGame example

---
+++ src/programmers/LV1/DartGame_exam.java
@@ -0,0 +1,45 @@
+package programmers.LV1;
+
+import java.util.Stack;
+
+public class DartGame_exam {
+    /* Stack */
+    public int solution(String dartResult) {
+        Stack<Integer> stack = new Stack<>();
+        int sum = 0;
+        for (int i = 0; i < dartResult.length(); ++i) {
+            char c = dartResult.charAt(i);
+            if (Character.isDigit(c)) {
+                // char to int
+                sum = (c - '0');
+                if (sum == 1 && i < dartResult.length() - 1 && dartResult.charAt(i + 1) == '0') {
+                    sum = 10;
+                    i++;
+                }
+                stack.push(sum);
+            } else {
+                int prev = stack.pop();
+                if (c == 'D') {
+                    prev *= prev;
+                } else if (c == 'T') {
+                    prev = prev * prev * prev;
+                } else if (c == '*') {
+                    if (!stack.isEmpty()) {
+                        int val = stack.pop() * 2;
+                        stack.push(val);
+                    }
+                    prev *= 2;
+                } else if (c == '#') {
+                    prev *= (-1);
+                }
+                // System.out.println(prev);
+                stack.push(prev);
+            }
+        }
+        int totalScore = 0;
+        while (!stack.isEmpty()) {
+            totalScore += stack.pop();
+        }
+        return totalScore;
+    }
+}
