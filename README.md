eccd1b53c1aeaf0d817bc43f6ce5640caedee186
Ju Minji <alswl0w0@gmail.com>
2023-02-18 18:23:19 +0900

    Compare List.contains() with Loop+break Statement

--- README.md
+++ README.md
@@ -14,8 +14,8 @@
 | LV2 | 17680 | [[1차] 캐시(2018 KAKAO BLIND RECRUITMENT)](https://school.programmers.co.kr/learn/courses/30/lessons/17680) |                                                               |
 |LV2 | 12909| [올바른 괄호(스택/큐)](https://school.programmers.co.kr/learn/courses/30/lessons/12909)                          |
 |LV2 | 12973| [짝 지어 제거하기(2017 팁스타운)](https://school.programmers.co.kr/learn/courses/30/lessons/12973)                  |
-|LV2|12985| [★예상 대진표(2017 팁스다운)](https://school.programmers.co.kr/learn/courses/30/lessons/12985)                    |
-|LV2|42885| [구명보트(Greedy)](https://school.programmers.co.kr/learn/courses/30/lessons/42885)                          |
+|LV2|12985| [★ 예상 대진표(2017 팁스다운)](https://school.programmers.co.kr/learn/courses/30/lessons/12985)                   |
+|LV2|42885| [★ 구명보트(Greedy)](https://school.programmers.co.kr/learn/courses/30/lessons/42885)                        |
 <br>
 
 #### LV1
--- src/concept/datastructure/ListPerformanceTest.java
+++ src/concept/datastructure/ListPerformanceTest.java
@@ -4,13 +4,13 @@ import java.util.*;
 
 public class ListPerformanceTest {
     public static void main(String[] args){
-        final int testDataSize = 500;
+        final int testDataSize = 5000;
         final int leftLimit = 97; // letter 'a'
         final int rightLimit = 122; // letter 'z'
         final int targetStringLength = 5;
         Random random = new Random();
-        ArrayList arList = new ArrayList<>();
-        LinkedList lkList = new LinkedList();
+        ArrayList<String> arList = new ArrayList<>();
+        LinkedList<String> lkList = new LinkedList();
 
         for(int i=0; i<testDataSize; i++){
             String ran = random.ints(leftLimit,rightLimit + 1)
@@ -23,14 +23,16 @@ public class ListPerformanceTest {
             lkList.add(ran);
         }
 
-        System.out.println("=====search by value======");
-        System.out.println("ArrayList = " + test_search_by_value(random,testDataSize, leftLimit, rightLimit, targetStringLength, arList));
-        System.out.println("LinkedList = " + test_search_by_value(random,testDataSize, leftLimit, rightLimit, targetStringLength, lkList));
+//        System.out.println("=====search by value======");
+//        System.out.println("ArrayList = " + test_search_by_value(random,testDataSize, leftLimit, rightLimit, targetStringLength, arList));
+//        System.out.println("LinkedList = " + test_search_by_value(random,testDataSize, leftLimit, rightLimit, targetStringLength, lkList));
+//
+//        System.out.println("\n=====get by index======");
+//        System.out.println("ArrayList = " + test_search_by_index(random,testDataSize, leftLimit, rightLimit, targetStringLength, arList));
+//        System.out.println("LinkedList = " + test_search_by_index(random,testDataSize, leftLimit, rightLimit, targetStringLength, lkList));
 
-        System.out.println("\n=====get by index======");
-        System.out.println("ArrayList = " + test_search_by_index(random,testDataSize, leftLimit, rightLimit, targetStringLength, arList));
-        System.out.println("LinkedList = " + test_search_by_index(random,testDataSize, leftLimit, rightLimit, targetStringLength, lkList));
-    }
+        System.out.println("=====contains() 메서드와 (루프문 + break문) 수행시간 비교하기======");
+        System.out.println("contains, loop+break = " + Arrays.toString(compare_contains_with_loop(random,testDataSize, leftLimit, rightLimit, targetStringLength, arList)));}
 
     private static long test_search_by_value(Random random, int testDataSize, int leftLimit, int rightLimit, int targetStringLength, List list){
         long start = System.currentTimeMillis();
@@ -58,4 +60,39 @@ public class ListPerformanceTest {
 
         return end - start;
     }
+
+    private static long[] compare_contains_with_loop(Random random, int testDataSize, int leftLimit, int rightLimit, int targetStringLength, ArrayList<String> list){
+        long[] answer = new long[2];
+        long contains_start = 0;
+        long contains_end = 0;
+        long loop_start = 0;
+        long loop_end = 0;
+
+        for(int i = 0; i < testDataSize; i++) {
+            // list size 안에서 랜덤숫자 생성
+            int ran = random.nextInt(list.size());
+            String target = list.get(ran);
+
+            contains_start += System.currentTimeMillis(); // contains() 수행시간 측정 시작
+            list.contains(target);
+            contains_end += System.currentTimeMillis(); // contains() 수행시간 측정 끝
+
+            answer[0] += contains_end - contains_start;
+
+            loop_start += System.currentTimeMillis(); // loop 수행시간 측정 시작
+            for(String element : list){
+                if(element.equals(target)) {
+                    break;
+                }
+            }
+            loop_end += System.currentTimeMillis(); // loop 수행시간 측정 끝
+
+            answer[1] += loop_end - loop_start;
+        }
+        answer[0] = answer[0]/ testDataSize;
+        answer[1] = answer[1]/ testDataSize;
+
+        return answer;
+    }
+
 }
