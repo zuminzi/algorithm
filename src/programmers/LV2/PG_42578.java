dacf874be35d74f024200a53d5ed2c4ebed5a677
Ju Minji <alswl0w0@gmail.com>
2023-02-20 22:55:01 +0900

    Fail PG_42578

--- src/programmers/LV2/PG_42578.java
+++ src/programmers/LV2/PG_42578.java
@@ -2,72 +2,55 @@ package programmers.LV2;
 
 import java.util.*;
 
-//  96.4/100.0, time out
+//  96.4/100.0, out of memory
 public class PG_42578 {
-    static int answer;
+    static boolean[] visited;
+    static List<Integer> output;
     public int solution(String[][] clothes) {
-        answer = 0;
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (int k = 0; k < clothes.length; k++) {
-            if (!map.containsKey(clothes[k][1])) {
-                List<String> items = new ArrayList<>();
-                items.add(clothes[k][0]);
-                map.put(clothes[k][1], items);
-            } else {
-                List<String> oldList = map.get(clothes[k][1]);
-                oldList.add(clothes[k][0]);
-                map.put(clothes[k][1], oldList);
-            }
+        int answer = 0;
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0; i< clothes.length; i++) { // containsKey로 검사할 필요 없이 merge로 Key가 없을 시 default value도 두 번째 인수에 넣어주면 ok
+            map.merge(clothes[i][1], 1, (oldValue, newValue) -> oldValue + 1);
         }
 
-        List <String> keyList = new ArrayList<>(map.keySet());
-
-
-        for (int i = 1; i <= map.size(); i++) {
-            if (i == 1) { // nCr에서 r이 1인 경우
+        visited = new boolean[map.size()];
+        List<String> keyList = new ArrayList<>(map.keySet());
+        for(int i=1; i<= map.size(); i++){
+            if(i==1) {
                 for (String key : map.keySet()) {
-                    answer += map.get(key).size();
-                }
-            } else if (i == map.size()){ // nCr에서 r이 n인 경우
-                int chooseAll = 1;
-                for (String key : map.keySet()) {
-                    chooseAll *= map.get(key).size();
-                }
-                if (chooseAll != 1) {
-                    answer += chooseAll;
+                    answer += map.get(key);
                 }
             } else {
-                boolean[] check = new boolean[map.size()];
-                combination_DFS(map, keyList, check, 0, 0, i);
+                output = new ArrayList<>();
+                combination(0, map.size(), i);
+                int sum = 1;
+                for (int k = 0; k < output.size(); k++) {
+                    sum *= map.get(keyList.get(output.get(k)));
+                    if((k+1) % i == 0) {
+                        answer += sum;
+                        sum = 1;
+                    }
+                }
             }
         }
-        return answer;
+    return answer;
     }
 
-    private void combination_DFS(HashMap<String, List<String>> map, List<String> keyList, boolean[] check, int idx, int cnt, int r) {
-        if (cnt == r) {
-            int sum = 1;
-            for(int i=0; i<check.length; i++) {
-                if (check[i]) {
-                        if(r == 1) {
-                        answer += map.get(keyList.get(i)).size();
-                        } else {
-                            sum *= map.get(keyList.get(i)).size();
-                        }
+    static void combination(int start, int n, int r) {
+        if(r == 0) {
+            for(int i=0; i< visited.length; i++){
+                if(visited[i]){
+                    output.add(i);
                 }
             }
-            if(r != 1) {
-                answer += sum;
-            }
+            return;
         }
 
-        for(int i=idx; i< check.length;i++) {
-            if(check[i]) {continue;}
-
-            check[i] = true;
-            combination_DFS(map, keyList, check, i, cnt+1, r);
-            check[i] = false;
-            }
+        for(int i=start; i<n; i++) {
+            visited[i] = true;
+            combination(i + 1, n, r - 1);
+            visited[i] = false;
+        }
     }
 
     public static void main(String[] args){
