1548191f163d8220fd44201668262cff2da487c4
Ju Minji <alswl0w0@gmail.com>
2023-04-17 22:42:55 +0900

    Add Example of BOJ_2021

--- src/baekjoon/BOJ_2021.java
+++ src/baekjoon/BOJ_2021.java
@@ -1,118 +1,95 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 // output : 최소 환승 횟수
 public class BOJ_2021 {
-    // time out
-    private static int start;
-    private static int end;
-    private static int[][] stations;
-    private static int min;
+    public static class Node {
+        int curLine;
+        int curStation;
+        int transCount;
 
-    public static void main(String[] args) {
-//        int totalStation = 10;
-//        int totalLine = 3;
-//        stations = new int[][]{{1, 2, 3, 4, 5}, {9, 7, 10}, {7, 6, 3, 8}};
-//        start = 1;
-//        end = 10;
-//        int[] arr = new int[]{0, 1, 2};
-        Scanner sc = new Scanner(System.in);
-        int totalStation = sc.nextInt();
-        int totalLane = sc.nextInt();
-        int[] arr = new int[totalLane];
-        stations = new int[totalLane][];
-
-        for(int i=0; i<totalLane; i++) {
-            Queue<Integer> q = new LinkedList();
-            while(true) {
-                int num = sc.nextInt();
-                if (num != -1) {
-                    q.add(num);
-                } else {
-                    break;
-                }
-            }
-            stations[i] = new int[q.size()];
-            int k =0;
-            while(!q.isEmpty()) {
-                stations[i][k++] = q.poll();
-            }
-            arr[i] = i;
-        }
-        start = sc.nextInt();
-        end = sc.nextInt();
-        min = Integer.MAX_VALUE;
-        perm(arr, new boolean[totalLane], new int[totalLane], 0);
-        if (min == Integer.MAX_VALUE) min = -1;
-        System.out.println(min);
-    }
-
-    private static void perm(int[] arr, boolean[] visited, int[] output, int depth) {
-        if (depth == visited.length) {
-            move(output);
-            return;
-        }
-
-        for (int i = 0; i < visited.length; i++) {
-            if (visited[i] != true) {
-                visited[i] = true;
-                output[depth] = arr[i];
-//                if(depth == 0){
-//                    if (!Arrays.stream(stations[output[0]]).anyMatch(station -> station == start)) {
-//                        visited[i] = false;
-//                        continue;
-//                    }
-//                }
-                perm(arr, visited, output, depth + 1);
-                visited[i] = false;
-            }
+        public Node(int curLine, int curStation, int transCount) {
+            this.curLine = curLine;
+            this.curStation = curStation;
+            this.transCount = transCount;
         }
     }
 
-    static class Pair {
-        int curRow; // 현재 stations row
-        int transfer; // 환승 횟수
-        int next; // next row
+    static int N, M;
+    static boolean[] visitedLine;
+    static boolean[] visitedStation;
+    static ArrayList<Integer>[] stations; // List 타입의 배열
+    static ArrayList<Integer>[] lines;
 
-        Pair(int curRow, int transfer, int next) {
-            this.curRow = curRow;
-            this.transfer = transfer;
-            this.next = next;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken()); // 역 개수
+        M = Integer.parseInt(st.nextToken()); // 노선 개수
+
+        visitedLine = new boolean[M + 1];
+        visitedStation = new boolean[N + 1];
+        stations = new ArrayList[N + 1];
+        lines = new ArrayList[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            stations[i] = new ArrayList<>();
+            lines[i] = new ArrayList<>();
         }
+
+        // 역 별로 포함된 노선 리스트(stations), 노선 별 포함하는 역 리스트(lines) 각각 관리
+        for (int i = 1; i <= M; i++) {
+            String[] s = br.readLine().split(" ");
+            for (String station : s) {
+                int statN = Integer.parseInt(station);
+                if (statN == -1) break;
+                stations[statN].add(i);
+                lines[i].add(statN);
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int answer = go(start, end);
+        System.out.println(answer);
     }
 
-    private static void move(int[] output) {
-        if (!Arrays.stream(stations[output[0]]).anyMatch(station -> station == start)) {
-            return;
+    private static int go(int start, int end) {
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(n -> n.transCount));
+        visitedStation[start] = true;
+        for (int line : stations[start]) {
+            pq.add(new Node(line, start, 0));
+            visitedLine[line] = true;
         }
-        Queue<Pair> queue = new LinkedList<>();
-        queue.offer(new Pair(output[0], 0, 1));
 
-        while (!queue.isEmpty()) {
-            Pair p = queue.poll();
-            int row = p.curRow;
-            int transfer = p.transfer;
-            int next = p.next;
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
 
-            if (Arrays.stream(stations[row]).anyMatch(station -> station == end)) {
-                min = Math.min(transfer, min);
-                return;
-            }
-            if (next == output.length) {
-                return;
+            if (now.curStation == end) {
+                return now.transCount;
             }
 
-            int nextRow = output[next];
-            for (int col = 0; col < stations[row].length; col++) {
-                for (int k = 0; k < stations[nextRow].length; k++) {
-                    if (stations[nextRow][k] == stations[row][col]) {
-                        transfer++;
-                        queue.offer(new Pair(nextRow, transfer, next + 1));
-                        break;
+            for (int nextStation : lines[now.curLine]) { // 해당 노선의 역 목록 탐색// BFS
+
+                if (!visitedStation[nextStation]) {
+                    visitedStation[nextStation] = true;
+                    pq.add(new Node(now.curLine, nextStation, now.transCount));
+
+                    for (int nextLine : stations[nextStation]) { // 해당 역의 노선 목록 탐색 // DFS
+                        if (!visitedLine[nextLine]) {
+                            visitedLine[nextLine] = true;
+                            pq.add(new Node(nextLine, nextStation, now.transCount + 1));
+                        }
                     }
                 }
             }
         }
+        return -1;
     }
 }
