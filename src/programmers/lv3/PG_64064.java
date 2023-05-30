1cca2cf868e5319bb484a26d77591dfce47fa8be
Ju Minji <alswl0w0@gmail.com>
2023-05-31 00:47:46 +0900

    Improve PG_64064

--- src/programmers/lv3/PG_64064.java
+++ src/programmers/lv3/PG_64064.java
@@ -8,106 +8,69 @@ import java.util.*;
  - 가리고자 하는 문자 하나에 '*' 문자 하나를 사용하였고 아이디 당 최소 하나 이상의 '*' 문자를 사용하였습니다.
  - Problem Point
    - 가능한 user_id 후보군(경우의 수) 만들 때, 조합이 아닌 순열로 접근
+ - 코드 개선 이후 속도 차이
+   - ~1487.85ms, 404MB -> ~134.49ms, 101MB
  */
 public class PG_64064 {
-    private static String[] banned_id;
-    private static int point;
     private static Set<String> combChecked;
+
     public int solution(String[] user_id, String[] banned_id) {
-        this.banned_id = banned_id;
-        point = 0;
         combChecked = new HashSet<>();
-        comb(user_id, banned_id.length, new boolean[user_id.length],0);
-        return point;
-    }
+        List<List<String>> candidates = new ArrayList<>();
+        generateCandidates(user_id, banned_id, new ArrayList<>(), candidates);
 
-    private void comb(String[] arr, int r, boolean[] visited, int depth) {
-        if(depth == r){
-            checkIfBanned(arr, visited);
-            return;
-        }
-
-        for(int i=0; i<arr.length; i++){
-            if(!visited[i]) {
-                visited[i] = true;
-                comb(arr, r, visited, depth + 1);
-                visited[i] = false;
-            }
-        }
-    }
-
-    private void checkIfBanned(String[] arr, boolean[] visited) {
-        List<String> bannedList = new ArrayList<>();
-        boolean[] used = new boolean[banned_id.length];
         int count = 0;
-        for(int i=0; i<arr.length; i++){
-            if(visited[i]){
-                bannedList.add(arr[i]);
-                for(int k=0; k<banned_id.length; k++){
-                    if(!used[k] && isBanned(arr[i], banned_id[k])){
-                        used[k] = true;
-                        count++;
-                        break;
-                    }
+        for (List<String> candidate : candidates) {
+            if (isBanned(candidate, banned_id)) {
+                // 3. candidate 요소 중에 순서만 다른 요소(즉, 동일하게 취급되어야 할 요소) 거르기
+                // 정렬 후 combChecked로 중복 요소 제외
+                Collections.sort(candidate);
+                String str = String.join(" ", candidate);
+                if (combChecked.add(str)) {
+                    count++;
                 }
             }
         }
-        if(count == banned_id.length){
-            Collections.sort(bannedList);
-            String str = String.join(" ", bannedList);
-            if(combChecked.add(str)) {
-                point++;
-            }
-        } else {
-            perm(bannedList.toArray(new String[0]), new String[bannedList.size()], new boolean[bannedList.size()], 0);
-        }
+        return count;
     }
 
-     private void perm(String[] arr, String[] output, boolean[] visited, int depth) {
-        if (depth == arr.length) {
-            checkIfBanned(output);
+    // 1. 조합 -> 순열 두 단계 거치지 말고, 바로 순열 알고리즘으로 경우의 수 구하기
+    private void generateCandidates(String[] user_id, String[] banned_id, List<String> current, List<List<String>> candidates) {
+        if (current.size() == banned_id.length) {
+            candidates.add(new ArrayList<>(current));
             return;
         }
 
-        for (int i=0; i<arr.length; i++) {
-            if (visited[i] != true) {
-                visited[i] = true;
-                output[depth] = arr[i];
-                perm(arr, output, visited, depth + 1);
-                visited[i] = false;;
+        for (String userId : user_id) {
+            // 2. 제재 아이디 매치 여부를 line 37 라인 이후에 확인하는 대신 여기에서 확인하여 불필요한 수행 시간 줄이기
+            if (!current.contains(userId) && matchesBanned(userId, banned_id[current.size()])) {
+                current.add(userId);
+                generateCandidates(user_id, banned_id, current, candidates);
+                current.remove(current.size() - 1);
             }
         }
     }
 
-    private void checkIfBanned(String[] output) {
-        List<String> bannedList = new ArrayList<>();
-        boolean[] used = new boolean[banned_id.length];
-        int count = 0;
-        for(int i=0; i<output.length; i++){
-                for(int k=0; k<banned_id.length; k++){
-                    if(!used[k] && isBanned(output[i], banned_id[k])){
-                        used[k] = true;
-                        count++;
-                        bannedList.add(output[i]);
-                        break;
-                    }
-                }
-            }
-        if(count == banned_id.length){
-            Collections.sort(bannedList);
-            String str = String.join(" ", bannedList);
-            if(combChecked.add(str)) {
-                point++;
-            }
-        }
-    }
-
-    private boolean isBanned(String userId, String bannedId) {
+    private boolean matchesBanned(String userId, String bannedId) {
         if (userId.length() != bannedId.length()) {
             return false;
         }
-        for (int i = 0; i < bannedId.length(); i++) {
+
+        for (int i = 0; i < userId.length(); i++) {
             if (bannedId.charAt(i) != '*' && bannedId.charAt(i) != userId.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isBanned(List<String> candidate, String[] banned_id) {
+        if (candidate.size() != banned_id.length) {
+            return false;
+        }
+
+        for (int i = 0; i < candidate.size(); i++) {
+            if (!matchesBanned(candidate.get(i), banned_id[i])) {
                 return false;
             }
         }
